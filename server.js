df0648d3ba78551ebdf2a12056cdade2b6741ffb
Ani <158804664+an619@users.noreply.github.com>
2024-11-30 02:32:47 +0400

    trying to host it

--- server.js
+++ server.js
@@ -20,4 +20,7 @@ io.on("connection", function (socket){
     });
 });
 
-server.listen(5000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
