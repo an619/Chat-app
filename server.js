1a3ee4c4bd325e654b640a2a80526ee6efbf013c
Ani <158804664+an619@users.noreply.github.com>
2024-11-30 12:02:29 +0400

    changed port

--- server.js
+++ server.js
@@ -20,7 +20,4 @@ io.on("connection", function (socket){
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+server.listen(5000)
